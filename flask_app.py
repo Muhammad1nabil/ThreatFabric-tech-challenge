2a9d0152031b0fa794f14753eeb7f5be6a9d7f0e
muhammad1nabil <muhammad1nabil@yahoo.com>
2022-04-17 23:01:34 +0200

    Flask app

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,3 @@
-import json
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
@@ -12,18 +11,21 @@ def predict_user():
     data = request.get_json()
     model_name = data['Model']
     if model_name.upper() not in ['SVM', 'RF', 'XGB']:
-        return jsonify(
-            msg=f'invalid model, ({model_name})',
-            status=400)
+        return jsonify(msg=f'invalid model, {model_name}'), 400
 
     model = pickle.load(open(f'{model_name.lower()}.model', 'rb'))
+    try:
 
-    record = np.array([data['HT']['Mean'], data['PPT']['Mean'], data['RPT']['Mean'], data['RRT']['Mean'],
-                       data['HT']['STD'], data['PPT']['STD'], data['RPT']['STD'], data['RRT']['STD']])
+        record = np.array([data['HT']['Mean'], data['PPT']['Mean'], data['RPT']['Mean'], data['RRT']['Mean'],
+                           data['HT']['STD'], data['PPT']['STD'], data['RPT']['STD'], data['RRT']['STD']]).reshape(1, 8)
 
-    prediction = model.predict(record.reshape(1, 8))
-    print(prediction)
-    return jsonify({'user': int(prediction)})
+        prediction = model.predict(record)
+
+        return jsonify({'user': int(prediction)})
+
+    except ValueError:
+        return jsonify(
+            msg=f'invalid data, {data}'), 400
 
 
 if __name__ == '__main__':
